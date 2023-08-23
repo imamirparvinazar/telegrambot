3159fd169ec817d0e4d6d1aae5b5c90fbd839706
imamirparvinazar <imamirparvinazar@gmail.com>
2023-08-23 12:46:34 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { Telegraf } = require('telegraf');
+const data = require("./data.json")
+const fs = require("fs")
 
 const bot = new Telegraf('6649237638:AAHyB1R45MjD1o7gc3sMDFwHhgBkzH4SRbM');
 
@@ -6,6 +8,11 @@ bot.command('start', ctx => {
     console.log(ctx.from)
     bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
     })
+})
+
+
+bot.on("message", ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, ctx.reply("Hello!"))
 })
 
 bot.startPolling()
