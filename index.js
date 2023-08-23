7d3fe88f10b082715d3a811b837f1fe9490d8c4e
imamirparvinazar <imamirparvinazar@gmail.com>
2023-08-23 12:47:44 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const { Telegraf } = require('telegraf');
-const data = require("./data.json")
-const fs = require("fs")
-
 const bot = new Telegraf('6649237638:AAHyB1R45MjD1o7gc3sMDFwHhgBkzH4SRbM');
 
 bot.command('start', ctx => {
