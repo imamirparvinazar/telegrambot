f24fef93348488a7a7644ae925020a4bfc752301
imamirparvinazar <imamirparvinazar@gmail.com>
2023-08-24 11:12:27 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ const { Telegraf } = require('telegraf');
 const bot = new Telegraf('6649237638:AAHyB1R45MjD1o7gc3sMDFwHhgBkzH4SRbM');
 
 bot.command('start', ctx => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Enter your name: ', {
-    })
-    bot.on("message", ctx => {
-        bot.telegram.sendMessage(ctx.chat.id, ctx.message)
-    })
+    bot.telegram.sendMessage(ctx.chat.id, "Hello World!", {} )
+    console.log(ctx.message)
 })
-
-
 
 bot.startPolling()
